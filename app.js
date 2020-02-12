46836c0863fd755d315f0c2aa648365c9f588767
pawinchio <pawinchio@yahoo.com>
2020-02-12 23:36:11 +0700

    render upload page in app.js

--- app.js
+++ app.js
@@ -46,5 +46,6 @@ app.post('/login', user.login);//call for login post
 app.get('/home/dashboard', user.dashboard);//call for dashboard page after login
 app.get('/home/logout', user.logout);//call for logout
 app.get('/home/profile',user.profile);//to render users profile
+app.get('/home/upload',user.upload);//to render users profile
 //Middleware
 app.listen(8080)
