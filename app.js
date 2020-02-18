b119dc203df215e3eed79f5a68373d0c140dfd89
pawinchio <pawinchio@yahoo.com>
2020-02-18 10:37:57 +0700

    new web with node.js

--- app.js
+++ app.js
@@ -1,14 +1,7 @@
-/**
-* Module dependencies.
-*/
-var express = require('express')
-  , routes = require('./routes')
-  , user = require('./routes/user')
-  , http = require('http')
-  , path = require('path');
-//var methodOverride = require('method-override');
-var session = require('express-session');
-var app = express();
+const express = require('express');
+const multer = require('multer');
+const ejs = require('ejs');
+const path = require('path');
 var mysql      = require('mysql');
 var bodyParser=require("body-parser");
 var connection = mysql.createConnection({
@@ -17,35 +10,88 @@ var connection = mysql.createConnection({
               password : '0805025529',
               database : 'tbofficeai'
             });
+            connection.connect();
  
-connection.connect();
- 
-global.db = connection;
- 
-// all environments
-app.set('port', process.env.PORT || 8080);
-app.set('views', __dirname + '/views');
+            global.db = connection;
+
+// Set The Storage Engine
+const storage = multer.diskStorage({
+  destination: './public/uploads/',
+  filename: function(req, file, cb){
+    cb(null,file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+  }
+});
+//============================upload&check====================================================
+// Init Upload
+const upload = multer({
+  storage: storage,
+  limits:{fileSize: 100000000},
+  fileFilter: function(req, file, cb){
+    checkFileType(file, cb);
+  }
+}).single('myImage');
+
+// Check File Type
+function checkFileType(file, cb){
+  // Allowed ext
+  const filetypes = /jpeg|jpg|png|gif/;
+  // Check ext
+  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+  // Check mime
+  const mimetype = filetypes.test(file.mimetype);
+
+  if(mimetype && extname){
+    return cb(null,true);
+  } else {
+    cb('Error: Images Only!');
+  }
+}
+
+//==============================================================
+
+// Init app
+const app = express();
+
+// EJS
 app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(session({
-              secret: 'keyboard cat',
-              resave: false,
-              saveUninitialized: true,
-              cookie: { maxAge: 60000 }
-            }))
- 
-// development only
- 
-app.get('/', routes.index);//call for main index page
-app.get('/signup', user.signup);//call for signup page
-app.post('/signup', user.signup);//call for signup post 
-app.get('/login', routes.index);//call for login page
-app.post('/login', user.login);//call for login post
-app.get('/home/dashboard', user.dashboard);//call for dashboard page after login
-app.get('/home/logout', user.logout);//call for logout
-app.get('/home/profile',user.profile);//to render users profile
-app.get('/home/upload',user.upload);//to render users profile
-//Middleware
-app.listen(8080)
+
+// Public Folder
+app.use(express.static('./public'));
+//============================reder page==============================
+app.get('/', (req, res) => res.render('login'));
+app.get('/index', (req, res) => res.render('index'));
+app.get('/upload', (req, res) => res.render('upload'));
+
+//===========================post upload===================================
+app.post('/upload', (req, res) => {
+  upload(req, res, (err) => {
+    if(err){
+      res.render('upload', {
+        msg: err
+      });
+    } else {
+      if(req.file == undefined){
+        res.render('upload', {
+          msg: 'Error: No File Selected!'
+        });
+      } else {
+        var sql ="INSERT INTO `Image`(`time`,`input_path`,`output_path`,`resut`, `user_id`) VALUES ('" + '2020-02-1515.10.05' + "','" + 'uploads/'+ "','" + '-' + "','" + 'processing' + "','" + 2 + "')";
+        var query = db.query(sql, function(err, result) {
+
+          message = "File Uploaded!";
+          res.render('index.ejs',{message: message});
+       });
+        
+          // res.render('upload', {
+        //   msg: 'File Uploaded!',
+        //   file: `uploads/${req.file.filename}`
+        // });
+
+      }
+    }
+  });
+});
+
+const port = 3000;
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
